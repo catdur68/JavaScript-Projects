fd99a86a9cceb255218da22f2d8fe89e912354dd
catdur68 <66445106+catdur68@users.noreply.github.com>
2020-08-18 11:19:22 -0700

    Commit Assignment p64

---
+++ Basic_JavaScript_Projects/JS/Basic_JavaScript_2.js
@@ -0,0 +1,5 @@
+function myFunction() {
+     var sentence = "It looks like each function";
+     sentence += " needs to be saved in its own JS file.";
+     document.getElementById("concatenate").innerHTML = sentence;
+}
