03809425852b19c303b94193d7c29084b1fb3f38
catdur68 <66445106+catdur68@users.noreply.github.com>
2020-08-19 14:04:13 -0700

    Commit Javascript & HTML _4 Assignment p92

---
+++ Basic_JavaScript_Projects/JS/Basic_JavaScript_4.js
@@ -0,0 +1,20 @@
+function stock_watch()
+{
+    var Stock = {
+        name: "Tesla",
+        ticker: "TSLA",
+        index: "NASDAQ",
+        sector: "Auto-Manuf",
+        dividend: "No",
+        Price_Earning: 971.07,
+        EPS: 1.94,
+        rating: "C"
+    };
+    //document.getElementById("Dictionary").innerHTML = Stock.Price_Earning;
+    delete stock.Price_Earning;
+    document.getElementById("Dictionary").innerHTML = Stock.Price_Earning;
+   
+}
+
+
+
